ec786d024a5dc0149dea8729a6e8c85def47fe48
HexCore99 <160425842+HexCore99@users.noreply.github.com>
2024-02-19 09:07:59 +0600

    prb10.c

--- 1D Array/prb10.c
+++ 1D Array/prb10.c
@@ -1,9 +1,45 @@
 #include <stdio.h>
-
+#define MAX 50
 int main() {
-  
- float arrayp[1,2,3,4,5,6,7][12];
-  
-  
+
+ int A[MAX];
+ int B[MAX];
+
+int n;
+scanf("%d", &n);
+
+for (int i = 0; i < n; i++)
+{
+    scanf("%d", &A[i]);
+}
+int m;
+scanf("%d", &m);
+for (int i = 0; i < m; i++)
+{
+    scanf("%d", &B[i]);
+}
+int max = (n>m)?n:m;
+
+for (int i = 0; i < max; i++)
+{
+    int temp = A[i];
+    A[i]= B[i];
+    B[i] = temp;
+}
+printf("Array B:\n");
+for (int i = 0; i < n; i++)
+{
+    printf("%d ", B[i]);
+}
+printf("\nArray A:\n");
+for (int i = 0; i < m; i++)
+{
+    printf("%d ", A[i]);
+}
+
+
+
+
+
     return 0;
 }
