ae8c9b6124c07b0490647371e1d8649f353209a8
HexCore99 <160425842+HexCore99@users.noreply.github.com>
2024-02-20 09:45:40 +0600

    Add files via upload

---
+++ 1D Array/prb16.c
@@ -0,0 +1,56 @@
+
+#include <stdio.h>
+
+int main() {
+ int n;
+ scanf("%d", &n); 
+ int arr1[n];
+ for (int i = 0; i <n; i++)
+ {
+    scanf("%d", &arr1[i]);
+ }
+ int m;
+ scanf("%d", &m); 
+int arr2[m];
+
+for (int i = 0; i < m; i++)
+{
+   scanf("%d", &arr2[i]);
+
+}
+
+int Union[m+n];
+int Union_Indx = 0;
+
+
+
+for (int i = 0; i < n; i++)
+{
+
+ int flag = 1; 
+   for (int j =0 ; j < m; j++)
+   {
+    if (arr1[i] == arr2[j])    // checking if arr2 element and arr1 element is same
+    {
+      flag = 0;
+      break;
+    }
+     
+   }
+  if (flag)
+  {
+   Union[Union_Indx++] = arr1[i]; // if arr2 element is not present int the arr1,
+                                  // then put that element into Union array
+
+  }
+   
+}
+
+for (int i = 0; i < Union_Indx; i++)
+{
+   printf("%d ", Union[i]);
+}
+
+ 
+    return 0;
+}
