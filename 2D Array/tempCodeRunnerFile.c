5fee9c4041faa3e5982e3918bd61f6672ba1245d
HexCore99 <160425842+HexCore99@users.noreply.github.com>
2024-02-20 10:35:42 +0600

    Add files via upload

---
+++ 2D Array/tempCodeRunnerFile.c
@@ -0,0 +1,13 @@
+ for (int i = 0; i < n; i++)
+//  {
+//     for (int j = 0; j < n; j++)
+//     {
+//         if (i == j )
+//         {
+//             printf("%d ", arr[i][j]);
+//         }
+        
+//     }
+    
+//  }
+// pri
